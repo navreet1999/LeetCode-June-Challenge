5534b0fef73b822db501421c4ab5dd387cbb1956
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2020-06-24 10:53:52 +0530

    day 23

---
+++ Count Complete Tree Node.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countNodes(TreeNode root) {
+        
+        if(root==null)
+        return 0;
+        
+    
+        int l=countNodes(root.left);
+        int r=countNodes(root.right);
+        
+        
+        return l+r+1;
+    }
+}
